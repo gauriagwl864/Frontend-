3a6d98d5430d5e28df9886a56ed2aba554ac6a99
gauriagwl864 <gouriagarwal864@gmail.com>
2025-11-05 11:40:17 +0530

    DomDay2

---
+++ HTML/Classwork/DomDay2/script.js
@@ -0,0 +1,7 @@
+// const list=document.getElementsByClassName("list-item");
+// console.log(list);
+// item(0).innerText="hello hello";
+
+const list=document.querySelector(".list-item");
+console.log(list);
+//const list=document.querySelectorAll(".list-item");
