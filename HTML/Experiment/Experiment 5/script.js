61acebfc6b4c8e74fbf0c86b361be675a883fa3f
gauriagwl864 <gouriagarwal864@gmail.com>
2025-11-19 00:01:54 +0530

    Exp5

---
+++ HTML/Experiment/Experiment 5/script.js
@@ -0,0 +1,17 @@
+//Task-1
+//alert("hello world,we are here!");
+
+
+// Task-2
+// const username = prompt("hello whats your name");
+// alert("nice to meet you");
+
+// Task-3
+// let age = prompt ("Enter your age");
+// let msg = (age >= 18) ? "Can vote" : "Cannot vote";
+// alert(msg); 
+
+Task-4
+let age = prompt ("Enter your age");
+let msg = (age >= 18) ? document.write("<h1>Can vote.</h1>") : document.write("<h1>Cannot vote.</h1>");
+alert(msg); 
